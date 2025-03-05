8dc18338022bad2f3f1dc01bb5498a05aecfbe53
karn-27 <mohitjoshiw235@gmail.com>
2025-03-05 22:02:51 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -5,3 +5,6 @@ response = requests.get(url)
 
 print("Status Code:", response.status_code)
 print("Response:", response.text)
+import os
+port = int(os.getenv("PORT", 3000))
+app.run(host="0.0.0.0", port=port)
