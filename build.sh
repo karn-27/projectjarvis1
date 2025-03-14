46181f2d4f839efc40fbefc0e13f7089d063ec96
karn-27 <mohitjoshiw@gmail.com>
2025-03-14 21:40:56 +0530

    Added PortAudio install script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Updating system..."
-apt-get update && apt-get install -y portaudio19-dev
-pip install sounddevice
+apt-get update && apt-get install -y portaudio19-dev  # PortAudio install karna zaroori hai
+pip install -r requirements.txt  # Saari dependencies install hongi
